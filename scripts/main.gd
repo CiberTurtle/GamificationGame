42781fcdd617d54251f2f5ae72fad2b367a693b4
CiberTurtle <rylydou@gmail.com>
2023-03-19 10:40:32 -0500

    viewports

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,17 @@
 class_name Main extends Node
 
+@onready var sub_viewport_container: SubViewportContainer = %SubViewportContainer
+
 func _enter_tree() -> void:
 	Globals.main = self
 	Globals.world = %World
+
+var current_level: Level
+func load_level(scene: PackedScene) -> void:
+	# remove previous level
+	if is_instance_valid(current_level):
+		current_level.queue_free()
+	
+	# spawn new level
+	var level := scene.instantiate() as Level
+	sub_viewport_container.stretch_shrink = level.size
