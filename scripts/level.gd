e4e450e8dffeae9b6e806bb0173a259606df6fae
Ciber_Turtle <rylydou@gmail.com>
2023-03-19 13:22:49 -0500

    Squashed commit of the following:
    commit 265da8de17a0abd042581621b03ef1bde913d434
    Author: Ciber_Turtle <rylydou@gmail.com>
    Date:   Sun Mar 19 13:21:36 2023 -0500
    
        Squashed commit of the following:
    
        commit 1e3794ab0dc8890657f08716625f20368e737968
        Author: BlakeBukowsky <blakebuk@gmail.com>
        Date:   Sun Mar 19 11:40:13 2023 -0500
    
            Bad boomerang
    
            A horribly made boomerang...
            Ill fix it later
            Might just need its own script
    
        commit 8f7a92950e89c0fb4cc280708d5d39c5f7ae04a0
        Author: BlakeBukowsky <blakebuk@gmail.com>
        Date:   Sun Mar 19 10:55:16 2023 -0500
    
            Added Part Melee Attack
    
            Makes it so that its a big less buggy and more straight forward when making melee weapon
    
    commit 44d23d253e058ac57e4bbd2f7567b6cc35cb3dd4
    Author: Ciber_Turtle <rylydou@gmail.com>
    Date:   Sun Mar 19 13:20:28 2023 -0500
    
        level loading
    
    commit 42781fcdd617d54251f2f5ae72fad2b367a693b4
    Author: CiberTurtle <rylydou@gmail.com>
    Date:   Sun Mar 19 10:40:32 2023 -0500
    
        viewports

---
+++ scripts/level.gd
@@ -0,0 +1,6 @@
+class_name Level extends Node2D
+
+@export_range(1, 10, 1) var size := 3
+
+@onready var width := 1920/size
+@onready var height := 1080/size
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,42 @@
 class_name Main extends Node
 
+@export var player_scene: PackedScene
+
+@onready var game_viewport: SubViewport = %GameViewport
+@onready var load_level_dialog: FileDialog = %LoadLevelDialog
+
 func _enter_tree() -> void:
 	Globals.main = self
 	Globals.world = %World
+
+func _ready() -> void:
+	Console.register('load', func(): load_level_dialog.show(); Console.close())
+	load_level_dialog.popup_centered()
+	load_level_dialog.title = "Pick a level to load - Press 'L' to open this again"
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_L):
+		load_level_dialog.show()
+
+var current_level: Level
+func load_level(scene: PackedScene) -> void:
+	# remove previous level and actors
+	for child in Globals.world.get_children():
+		child.queue_free()
+	
+	# spawn new level
+	var level := scene.instantiate() as Level
+	Globals.world.add_child(level)
+	current_level = level
+	game_viewport.size_2d_override.x = current_level.width
+	game_viewport.size_2d_override.y = current_level.height
+	
+	spawn_players()
+
+func spawn_players() -> void:
+	var player := player_scene.instantiate() as Player
+	player.global_position = current_level.find_child('PlayerSpawns').get_child(0).position
+	Globals.world.add_child(player)
+
+func _on_load_level_dialog_file_selected(path: String) -> void:
+	load_level(load(path))
