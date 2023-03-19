4822e6582583fe148f72837467eba4b59dbb83b1
BlakeBukowsky <blakebuk@gmail.com>
2023-03-19 18:22:49 -0500

    Skew is now curve

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -4,7 +4,6 @@ const TPS := 60.
 
 @export_range(0, 60, 1, 'or_greater', 'suffix:ticks')  var lifetime := 60
 @onready var lifetime_sec := lifetime/TPS
-@export var skew_amount :=0.0
 var age := 0.0
 
 @export var speed_over_lifetime: Curve
@@ -12,10 +11,16 @@ var age := 0.0
 @export_range(0, 10, 1, 'or_greater', 'suffix:count')  var max_hits := 1
 var hits := 0
 var damage_source: Player
-@onready var proj_skew = randf_range(-skew_amount, skew_amount)
+@export var random_skew:=false
+@export var skew_curve:Curve
+var proj_skew:float
 
 func _ready() -> void:
 	var hitboxes := find_children('*', 'Hitbox')
+	if random_skew:
+		proj_skew = randf_range(-1, 1)
+	else:
+		proj_skew = 1
 	for hitbox in hitboxes:
 		hitbox.attack.connect(
 		func(node: Node2D):
@@ -27,7 +32,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var age_ratio := age/lifetime_sec
 	var speed := speed_over_lifetime.sample_baked(age_ratio)
-	var motion := Vector2(transform.basis_xform(Vector2.RIGHT).x, proj_skew).normalized() * delta * speed
+	var motion := Vector2(transform.basis_xform(Vector2.RIGHT).x, skew_curve.sample_baked(age_ratio) * proj_skew).normalized() * delta * speed
 	var hit := move_and_collide(motion)
 	
 	if hit:
