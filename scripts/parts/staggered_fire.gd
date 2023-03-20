1bdbb770dca9bb2417965cfde5d228143a008a10
Blake Bukowsky <blakebuk@gmail.com>
2023-03-20 14:41:41 -0500

    Changed projectile Speed, Added SMG

--- scripts/parts/staggered_fire.gd
+++ scripts/parts/staggered_fire.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var scene: PackedScene
 @export_range(0, 10, 1, 'or_greater', 'suffix:count') var count := 1
 @export var delay:=.1
+@export var durability:=1
 var til_spawn = 0.0
 var shots_left
 #@export var spread: Range
@@ -10,6 +11,7 @@ var shots_left
 func trigger() -> void:
 	til_spawn = 0.0
 	shots_left = count
+	durability -= 1
 
 func spawn() -> void:
 	var proj = scene.instantiate() as Node2D
@@ -19,7 +21,9 @@ func spawn() -> void:
 
 func _process(delta):
 	til_spawn -= delta
-	if shots_left > 0 && til_spawn < 0:
+	if shots_left && shots_left > 0 && til_spawn < 0:
 		spawn()
 		til_spawn = delay
 		shots_left -= 1
+		if shots_left <= 0 && durability <= 0:
+			owner.die()
