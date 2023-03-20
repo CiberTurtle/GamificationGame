c52925ac06cf280a10351a2f44b34fb65b846466
Blake Bukowsky <blakebuk@gmail.com>
2023-03-20 13:07:48 -0500

    Fixed knockback

--- scripts/parts/knockback.gd
+++ scripts/parts/knockback.gd
@@ -6,7 +6,7 @@ func _ready():
 	get_parent().attack.connect(attack)
 func attack(victim: Node2D) -> void:
 	if victim is Player:
-		victim.speed_extra += x_knockback
+		victim.speed_extra += x_knockback * owner.transform.basis_xform(Vector2.RIGHT).x
 		victim.speed_vertical += y_knockback
 	else:
-		victim.velocity += Vector2(x_knockback, y_knockback)
+		victim.velocity += Vector2(x_knockback * owner.transform.basis_xform(Vector2.RIGHT).x, y_knockback)
